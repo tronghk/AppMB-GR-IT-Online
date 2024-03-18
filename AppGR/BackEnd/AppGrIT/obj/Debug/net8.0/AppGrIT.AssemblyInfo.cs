9a78e013b037f551a51a63bd78c03aac67da7fc2
tronghk <trongnguyen.02stu@gmail.com>
2024-03-18 20:29:18 +0700

    complete register

--- AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
+++ AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45fafb867103f0af1b2013a38acc74b881fa66e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533d8c9cb145739fa38beff3758cd6e6ca6b254d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
