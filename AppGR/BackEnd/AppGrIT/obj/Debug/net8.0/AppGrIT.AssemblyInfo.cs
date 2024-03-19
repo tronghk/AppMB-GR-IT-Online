60cf7d84b8312ab77663217cd177b0a4ca7f88f5
tronghk <trongnguyen.02stu@gmail.com>
2024-03-19 12:01:21 +0700

    commit push

--- AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
+++ AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d76187ab3ed8048bc49cd4611c9054989a7ed13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad664e01af965028471750be735694062c5397f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
