a542d90e149c768112090a229063fe9cd4d9f79d
TranVinhTanDat <tandat27012002.td@gmail.com>
2024-03-24 23:18:27 +0700

    Create file xml for activity_create_maketplace  activity_create_post   activity_detail_marketplace   activity_list_user_like

---
+++ AppGR/FontEnd/app/src/main/java/com/example/appgrit/activities/ListUserLike.java
@@ -0,0 +1,16 @@
+package com.example.appgrit.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appgrit.R;
+
+public class ListUserLike extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_user_like);
+    }
+}
