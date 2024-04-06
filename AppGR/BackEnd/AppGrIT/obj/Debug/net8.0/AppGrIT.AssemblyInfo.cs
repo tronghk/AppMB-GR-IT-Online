20ca4521f4145367e4c7dbab995d769567e5f712
hoangkhang28902 <hoangkhang28902@gmail.com>
2024-04-06 14:02:45 +0700

    Sua loi thong bao DELETE_SUCCESS nhung khong xoa UserFriend

--- AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
+++ AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8df902291219d39aa993e32503341cdd4d9f7c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256b4a1ac8e5eceaa00b2e306937eaa9240de40b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
