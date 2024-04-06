1cae50ff19d865b5ffafcac4e837513717cf6e27
hoangkhang28902 <hoangkhang28902@gmail.com>
2024-04-06 14:38:01 +0700

    hoan thanh controller an nguoi dung

--- AppGR/BackEnd/AppGrIT/Controllers/UnUserController.cs
+++ AppGR/BackEnd/AppGrIT/Controllers/UnUserController.cs
@@ -1,8 +1,11 @@
 ﻿using AppGrIT.Helper;
 using AppGrIT.Model;
+using AppGrIT.Models;
 using AppGrIT.Services;
 using AppGrIT.Services.AppGrIT.Services;
+using Firebase.Auth;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppGrIT.Controllers
@@ -12,10 +15,12 @@ namespace AppGrIT.Controllers
     public class UnUserController : ControllerBase
     {
         private IUnUser _unUserManager;
+        private IUsers _userManager;
 
-        public UnUserController(IUnUser pression)
+        public UnUserController(IUnUser pression, IUsers user)
         {
             _unUserManager = pression;
+            _userManager = user;
         }
         [HttpGet("/count-unUser")]
         public async Task<IActionResult> CountUnUser(string userId)
@@ -43,6 +48,29 @@ namespace AppGrIT.Controllers
             {
                 var result = await _unUserManager.GetListUnUser(userId);
                 return Ok(result);
+            }
+            return NotFound();
+        }
+        [HttpPost("/add-UnUser")]
+        public async Task<IActionResult> AddUnUser([FromBody] UnUserModel model)
+        {
+            var user = await _userManager.GetUserToUserId(model.UserId!);
+            var userfr = await _userManager.GetUserToUserId(model.UnUserId!);
+
+            var us = await _unUserManager.GetUnUser(model.UserId, model.UnUserId);
+
+            if (user != null && userfr != null && us == null)
+            {
+                var result = await _unUserManager.CreateUnUser(model);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                return BadRequest(new ResponseModel
+                {
+                    Status = StatusResponse.STATUS_ERROR,
+                    Message = MessageResponse.MESSAGE_CREATE_FAIL
+                });
             }
             return NotFound();
         }
--- AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
+++ AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256b4a1ac8e5eceaa00b2e306937eaa9240de40b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ca4521f4145367e4c7dbab995d769567e5f712")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
