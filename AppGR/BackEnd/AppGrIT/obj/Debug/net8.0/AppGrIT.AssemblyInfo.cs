324df25fe8c86ddc557631f696a6900eaa6b9dce
tronghk <trongnguyen.02stu@gmail.com>
2024-04-19 21:28:20 +0700

    change branch

--- AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
+++ AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11523835ea7ab7687d3a2cc6983921ff5b28e225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c893be0ca0e32a93100db87a2af23699ecb56a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
