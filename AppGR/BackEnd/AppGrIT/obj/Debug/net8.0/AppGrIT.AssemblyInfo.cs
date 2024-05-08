2d807dc4d94abb53c9a51942a7bca47e9dd13e07
tronghk <trongnguyen.02stu@gmail.com>
2024-05-08 11:01:05 +0700

    fix thanh toan

--- AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
+++ AppGR/BackEnd/AppGrIT/obj/Debug/net8.0/AppGrIT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7863e192b114ad4c80fcd898e4ef0ce5507a816c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda6908f83d43b390a3262256fdf032b124ebac5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGrIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
